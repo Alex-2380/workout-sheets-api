8932e023d17f7e385471133f548a7b8de66bc48d
Alex-2380 <ajerickson12@gmail.com>
2025-08-16 13:09:52 -0600

    Update workout.js

--- workout-app/pages/workout.js
+++ workout-app/pages/workout.js
@@ -1,38 +1,16 @@
-import { useState, useEffect } from 'react';
-import { useRouter } from 'next/router';
-import ExerciseCard from '../components/ExerciseCard';
-import WorkoutSummaryPopup from '../components/WorkoutSummaryPopup';
 import Timer from '../components/Timer';
 import PlateCalculator from '../components/PlateCalculator';
 import OneRepMaxCalculator from '../components/OneRepMaxCalculator';
 
-export default function Workout({ theme }) {
-  const router = useRouter();
-  const [exercises, setExercises] = useState([]);
-  const [showSummary, setShowSummary] = useState(false);
-  const user = localStorage.getItem('user');
-
-  useEffect(() => {
-    if (!user) router.push('/');
-    fetch(`/api/sheets?tab=Data(${user})`)
-      .then(res => res.json())
-      .then(data => setExercises(data))
-      .catch(err => console.error(err));
-  }, []);
-
+export default function Workout() {
   return (
     <div className="container">
       <h1>Workout</h1>
-      {exercises.map((ex, i) => (
-        <ExerciseCard key={i} exercise={ex} />
-      ))}
-      <button onClick={() => setShowSummary(true)}>Finish Workout</button>
-      {showSummary && <WorkoutSummaryPopup onClose={() => setShowSummary(false)} />}
-
-    <div className="tools">
-      <Timer />
-      <PlateCalculator />
-      <OneRepMaxCalculator />
-    </div>
-  );
+      <div className="tools">
+        <Timer />
+        <PlateCalculator />
+        <OneRepMaxCalculator />
+      </div>
+    </div> // <-- make sure this closing div exists
+  );      // <-- make sure parentheses close
 }
