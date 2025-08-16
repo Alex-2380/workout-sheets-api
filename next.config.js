7d46adc912f627b132edf7fcf311c2ce4bc59a65
Alex-2380 <ajerickson12@gmail.com>
2025-08-16 15:54:40 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  // Export static pages cleanly (we only rely on client-side fetch for Sheets)
+  output: 'standalone',
+  images: { unoptimized: true }
 };
 
 module.exports = nextConfig;
